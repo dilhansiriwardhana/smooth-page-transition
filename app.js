4ec6c7bf70924c05748b03a8ab0e23342e83ba10
Dilhan <kanchanasiriwardhana1@gmail.com>
2023-12-07 14:32:53 +0530

    animation done

--- app.js
+++ app.js
@@ -36,22 +36,23 @@ const leaveAnimation = (current, done) => {
   );
 };
 
-const enterAnimation = (current, done) => {
+const enterAnimation = (current, done, gradient) => {
   const product = current.querySelector(".image-container");
   const text = current.querySelector(".showcase-text");
   const circles = current.querySelectorAll(".circle");
   const arrow = current.querySelector(".showcase-arrow");
 
   return (
-    (tlLeave.fromTo(arrow, { opacity: 0, y: 50 }, { opacity: 1, y: 0 }),
-    tlLeave.fromTo(
+    (tlEnter.fromTo(arrow, { opacity: 0, y: 50 }, { opacity: 1, y: 0 }),
+    tlEnter.fromTo(
       product,
       { y: -100, opacity: 0 },
       { y: 0, opacity: 1, onComplete: done },
       "<"
     ),
-    tlLeave.fromTo(text, { y: 100, opacity: 0 }, { opacity: 1, y: 0 }, "<")),
-    tlLeave.fromTo(
+    tlEnter.to("body", { background: gradient }, "<"),
+    tlEnter.fromTo(text, { y: 100, opacity: 0 }, { opacity: 1, y: 0 }, "<")),
+    tlEnter.fromTo(
       circles,
       { y: -200, opacity: 0 },
       {
@@ -73,6 +74,13 @@ barba.init({
     // Showcase transition
     {
       name: "default",
+      once(data) {
+        const done = this.async();
+        let next = data.next.container;
+        let gradient = getGradient(data.next.namespace);
+        gsap.set("body", { background: gradient });
+        enterAnimation(next, done, gradient);
+      },
       leave(data) {
         const done = this.async();
         let current = data.current.container;
@@ -81,8 +89,49 @@ barba.init({
       enter(data) {
         const done = this.async();
         let next = data.next.container;
-        enterAnimation(next, done);
+        let gradient = getGradient(data.next.namespace);
+        enterAnimation(next, done, gradient);
+      },
+    },
+    {
+      // Product-page animation
+      name: "product-transition",
+      from: { namespace: ["handbag"] },
+      to: { namespace: ["product"] },
+      enter(data) {
+        const done = this.async();
+        let next = data.next.container;
+        productEnterAnimation(next, done);
+      },
+      leave(data) {
+        const done = this.async();
+        let current = data.current.container;
+        productLeaveAnimation(current, done);
       },
     },
   ],
 });
+
+function productEnterAnimation(next, done) {
+  tlEnter.fromTo(next, { y: "100%" }, { y: "0%" });
+  tlEnter.fromTo(
+    ".card",
+    { opacity: 0, y: 50 },
+    { opacity: 1, y: 0, stagger: 0.1, onComplete: done }
+  );
+}
+
+function productLeaveAnimation(current, done) {
+  tlLeave.fromTo(current, { opacity: 1 }, { opacity: 0, onComplete: done });
+}
+
+function getGradient(name) {
+  switch (name) {
+    case "handbag":
+      return "linear-gradient(260deg, #b75d62, #754d4f)";
+    case "boot":
+      return "linear-gradient(260deg, #5d8cb7, #4c4f70)";
+    case "hat":
+      return "linear-gradient(260deg, #b27a5c, #a7f5450)";
+  }
+}
